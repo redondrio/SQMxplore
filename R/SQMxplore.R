d65dfe22d05dbfe9711b0383fb4e85257ba01117
Alvaro Redondo <Redoned@users.noreply.github.com>
2022-04-26 11:47:31 +0200

    Set imports

--- R/SQMxplore.R
+++ R/SQMxplore.R
@@ -1,13 +1,3 @@
-# Libraries ----
-library(shiny)
-library(SQMtools)
-library(ggplot2)
-library(DT)
-library(data.table)
-library(vegan)
-library(caret)
-library(compositions)
-
 # Create Shiny app
 SQMxplore <- function(){
     source("app.R")
