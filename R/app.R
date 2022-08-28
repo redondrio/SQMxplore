18eaffcb02e1013117c74875e61ffe18499e96a0
Alvaro Redondo <Redoned@users.noreply.github.com>
2022-08-28 10:32:22 +0200

    Package can be installed

--- R/app.R
+++ R/app.R
@@ -1,22 +1,18 @@
 # Shiny app
-# Sourced code ----
-source("generateUI.R")
 # Global variables ----
 SQM <<- NULL
 samples_path <<- "../shiny_input/" #input directory with samples as subdirectories
 # sample tables are expected to be extracted and parsed with extract_lite.sh
 data_path <<- "../shiny_data/" #input directory with data and metadata tables
-# UI ----
-ui <- generateUI()
   
 # Server ----
 server <- function(input, output, clientData, session) {
   # Increase maximum upload size
   options(shiny.maxRequestSize=10*1024^3)
-  
+
   # Reactive values to store data
   reactiveData <- reactiveValues()
-  
+
   # Load the SQM object and stats files ----
   proj_dir <- reactive(paste0(samples_path,input$project,"/"))
   observeEvent(input$proj_load,{
@@ -59,13 +55,13 @@ server <- function(input, output, clientData, session) {
                       choices = names(reactiveData$SQM[["taxa"]])
     )
   }) # Close observer
-  
+
   observe({
     updateSelectInput(session, "count_tax",
                       choices = names(reactiveData$SQM[["taxa"]][[input$rank_tax]])
     )
   }) # Close observer
-  
+
   observe({
     updateNumericInput(session, "n_tax",
                       max = length(unique(rownames(
@@ -79,11 +75,11 @@ server <- function(input, output, clientData, session) {
     )
   }) # Close observer
 
-  observe({updateSelectizeInput(session,"samples_tax", 
+  observe({updateSelectizeInput(session,"samples_tax",
                        choices = reactiveData$SQM$misc$samples,
                        selecte = reactiveData$SQM$misc$samples
   )})
-  
+
   # Output Taxonomy ----
   reactTaxPlot <- reactive({
     if (input$sel_tax){
@@ -113,46 +109,46 @@ server <- function(input, output, clientData, session) {
                    rescale = input$rescale_tax,
                    base_size = input$base_size_tax)
     }
-  })  
-  
+  })
+
   output$taxPlot <- renderPlot({
     print(reactTaxPlot())
   })
-  
+
   output$taxPlotDown <- downloadHandler(
     filename = function() {paste("Tax_plot_", Sys.time(), '.pdf', sep='') },
     content = function(file) {
       ggsave(file,reactTaxPlot(),device="pdf")
     }
   )
-  
+
   # Update Functions Inputs ----
   observe({
-    updateSelectInput(session, "fun_level_fun", 
+    updateSelectInput(session, "fun_level_fun",
                       choices = names(reactiveData$SQM$functions),
     )
   })
-  
+
   observe({
     updateSelectizeInput(session, "count_fun",
                          choices = names(reactiveData$SQM[["functions"]][[input$fun_level_fun]])
     )
   })
-  
+
   observe({
-    updateSelectizeInput(session, "samples_fun", 
+    updateSelectizeInput(session, "samples_fun",
                          choices = reactiveData$SQM$misc$samples,
                          selected = reactiveData$SQM$misc$samples
     )
   })
-  
+
   observe({
     updateNumericInput(session, "n_fun",
                       max = length(unique(rownames(
                         reactiveData$SQM[["functions"]][[input$fun_level_fun]][[input$count_fun]])))
     )
   })
-  
+
   observe({
     updateSelectizeInput(session, "fun_fun",
                          choices = unique(rownames(
@@ -160,7 +156,7 @@ server <- function(input, output, clientData, session) {
                          server = TRUE
     )
   })
-  
+
   observeEvent(input$fun_level_fun,{
     updateSelectizeInput(session, 'fun_fun',
                          choices = rownames(
@@ -190,20 +186,20 @@ server <- function(input, output, clientData, session) {
                     base_size = input$base_size_fun)
     }
   })
-  
+
   output$funPlot <- renderPlot({
     print(reactFunPlot())
   })
-  
+
   output$funPlotDown <- downloadHandler(
     filename = function() {
-      paste("Fun_plot_", Sys.time(), '.pdf', sep='') 
+      paste("Fun_plot_", Sys.time(), '.pdf', sep='')
     },
     content = function(file) {
       ggsave(file, reactFunPlot(), device="pdf")
     }
   )
-  
+
   # Update Table Inputs ----
   observe({updateSelectInput(session,"lev1_tab",
                              choices = switch(class(reactiveData$SQM),
@@ -213,14 +209,14 @@ server <- function(input, output, clientData, session) {
                              ),
                              selected = ""
   )})
-  
+
   observeEvent(input$lev1_tab,{
     updateSelectInput(session,"lev2_tab",
                       choices = names(reactiveData$SQM[[input$lev1_tab]]),
                       selected = ""
     )
   }) # Close observer
-  
+
   observeEvent(c(input$lev1_tab,input$lev2_tab),{
     updateSelectInput(session,"lev3_tab",
                       choices = switch(input$lev1_tab,
@@ -232,7 +228,7 @@ server <- function(input, output, clientData, session) {
                       selected = ""
     )
   }) # Close lev1+2 observer
-  
+
   observeEvent(input$lev3_tab,{
     updateSelectizeInput(session,"cols_tab",
                          choices = switch(input$lev1_tab,
@@ -251,7 +247,7 @@ server <- function(input, output, clientData, session) {
                          )
     )
   }) # Close lev3 observer
-  
+
   # Output Table ----
   reactTable <- reactive({
     table <- switch(input$lev1_tab,
@@ -261,7 +257,7 @@ server <- function(input, output, clientData, session) {
                     'taxa'      = as.data.frame(reactiveData$SQM[[input$lev1_tab]][[input$lev2_tab]][[input$lev3_tab]][,input$cols_tab]),
                     'functions' = as.data.frame(reactiveData$SQM[[input$lev1_tab]][[input$lev2_tab]][[input$lev3_tab]][,input$cols_tab])
     )
-    
+
     if (input$lev2_tab %in% c("KEGG","COG")){
       annot <- switch(input$lev2_tab,
                       "KEGG" = "KEGG_names",
@@ -274,11 +270,11 @@ server <- function(input, output, clientData, session) {
     }
     table
   })
-  
+
   output$table <- DT::renderDataTable({
     DT::datatable(reactTable())
   })
-  
+
   output$tabDown <- downloadHandler(
     filename = function() {
       paste("Table", Sys.time(), ".csv", sep = "")
@@ -291,7 +287,7 @@ server <- function(input, output, clientData, session) {
   observe({
     updateSelectInput(session,"orfs_row1",
                       choices = reactiveData$orfs_st[,1]
-    )        
+    )
     updateSelectInput(session,"orfs_row2",
                       choices = reactiveData$orfs_st[,1]
     )
@@ -301,13 +297,13 @@ server <- function(input, output, clientData, session) {
   reactReadsSum <- reactive({ #create reactive function
     as.data.frame(reactiveData$reads_st[,-1],row.names=reactiveData$reads_st[,1])
   })
-  output$reads_sum <- DT::renderDataTable({ #generate output 
+  output$reads_sum <- DT::renderDataTable({ #generate output
     DT::datatable(reactReadsSum(),options=list(
-      paging=FALSE, searching=FALSE  
+      paging=FALSE, searching=FALSE
     )
     )
   })
-  
+
   # Barplots from Reads
   output$reads_readbar <- renderPlot({
     barplot(as.matrix(reactReadsSum()[1,! names(reactReadsSum()) %in% c("Assembly")]), main="Reads", col="#008080", las=2)
@@ -315,61 +311,61 @@ server <- function(input, output, clientData, session) {
   output$reads_basebar <- renderPlot({
     barplot(as.matrix(reactReadsSum()[2,! names(reactReadsSum()) %in% c("Assembly")]), main="Bases", col="#00CED1", las=2)
   })
-  
+
   # Contigs
   reactContigsSum <- reactive({ #create reactive function
     df <- as.data.frame(reactiveData$contigs_st[,-1],row.names=reactiveData$contigs_st[,1])
     if (ncol(df)==1){names(df) <- c("Value")}
     df
   })
-  output$contigs_sum <- DT::renderDataTable({ #generate output 
+  output$contigs_sum <- DT::renderDataTable({ #generate output
     DT::datatable(reactContigsSum(),options=list(
-      paging=FALSE, searching=FALSE  
+      paging=FALSE, searching=FALSE
     )
     )
   })
-  
+
   # Taxa
   reactTaxaSum <- reactive({ #create reactive function
     as.data.frame(reactiveData$taxa_st[,-1],row.names=reactiveData$taxa_st[,1])
   })
-  output$taxa_sum <- DT::renderDataTable({ #generate output 
+  output$taxa_sum <- DT::renderDataTable({ #generate output
     DT::datatable(reactTaxaSum(),options=list(
-      paging=FALSE, searching=FALSE  
+      paging=FALSE, searching=FALSE
     )
     )
   })
-  
+
   # Orfs
   reactOrfsSum <- reactive({ #create reactive function
     as.data.frame(reactiveData$orfs_st[,-1],row.names=reactiveData$orfs_st[,1])
   })
-  output$orfs_sum <- DT::renderDataTable({ #generate output 
+  output$orfs_sum <- DT::renderDataTable({ #generate output
     DT::datatable(reactOrfsSum(),options=list(
-      paging=FALSE, searching=FALSE  
+      paging=FALSE, searching=FALSE
     )
     )
   })
-  
+
   # Barplots from Orfs
   output$orfs_bar1 <- renderPlot({
-    barplot(as.matrix(reactOrfsSum()[input$orfs_row1,]), main=input$orfs_row1, 
+    barplot(as.matrix(reactOrfsSum()[input$orfs_row1,]), main=input$orfs_row1,
             col="#FF7F50", ylim=c(0,max(reactOrfsSum()[input$orfs_row1,])), las=2)
   })
   output$orfs_bar2 <- renderPlot({
-    barplot(as.matrix(reactOrfsSum()[input$orfs_row2,]), main=input$orfs_row2, 
+    barplot(as.matrix(reactOrfsSum()[input$orfs_row2,]), main=input$orfs_row2,
             col="#4169E1", las=2)
   })
-  
+
   # Bins
   reactBinsSum <- reactive({ #create reactive function
     df <- as.data.frame(reactiveData$bins_st[,-1],row.names=reactiveData$bins_st[,1])
     if (ncol(df)==1){names(df) <- c("Value")}
     df
   })
-  output$bins_sum <- DT::renderDataTable({ #generate output 
+  output$bins_sum <- DT::renderDataTable({ #generate output
     DT::datatable(reactBinsSum(),options=list(
-      paging=FALSE, searching=FALSE  
+      paging=FALSE, searching=FALSE
     )
     )
   })
@@ -380,14 +376,14 @@ server <- function(input, output, clientData, session) {
                       selected = ""
     )
   }) # Close lev1 observer
-  
+
   observeEvent(input$lev2_ma,{
     updateSelectInput(session,"lev3_ma",
                       choices = names(reactiveData$SQM[[input$lev1_ma]][[input$lev2_ma]]),
                       selected = ""
     )
   }) # Close lev2 observer
-  
+
   observeEvent(input$lev3_ma,{
     updateSliderInput(session, 'n_ma', value = 2,
                       max = length(unique(rownames(
@@ -398,28 +394,28 @@ server <- function(input, output, clientData, session) {
                          choices = rownames(
                            reactiveData$SQM[[input$lev1_ma]][[input$lev2_ma]][[input$lev3_ma]]))
   }) # Close lev3 observer
-  
+
   observeEvent(input$load_ma,{
-    updateSelectizeInput(session,"samples_ma", 
+    updateSelectizeInput(session,"samples_ma",
                          choices = reactiveData$SQM$misc$samples,
                          selected = reactiveData$SQM$misc$samples
     )
   }) # Close load_ma observer
-  
+
   observeEvent(input$lev1_const_ma,{
     updateSelectInput(session,"lev2_const_ma",
                       choices = names(reactiveData$SQM[[input$lev1_const_ma]]),
                       selected = ""
     )
   }) # Close lev1 observer
-  
+
   observeEvent(input$lev2_const_ma,{
     updateSelectInput(session,"lev3_const_ma",
                       choices = names(reactiveData$SQM[[input$lev1_const_ma]][[input$lev2_const_ma]]),
                       selected = ""
     )
   }) # Close lev2 observer
-  
+
   observeEvent(input$lev3_const_ma,{
     updateSliderInput(session, 'n_const_ma', value = 2,
                       max = length(unique(rownames(
@@ -430,56 +426,56 @@ server <- function(input, output, clientData, session) {
                          choices = rownames(
                            reactiveData$SQM[[input$lev1_const_ma]][[input$lev2_const_ma]][[input$lev3_const_ma]]))
   }) # Close lev3 observer
-  
+
   observeEvent(input$load_const_ma,{
-    updateSelectizeInput(session,"samples_const_ma", 
+    updateSelectizeInput(session,"samples_const_ma",
                          choices = reactiveData$SQM$misc$samples,
                          selected = reactiveData$SQM$misc$samples
     )
   }) # Close load_const_ma observer
-  
+
   # Auxiliary Multivariate Analysis ----
   # To add a new analysis, these are the steps:
   #     1. Add the analysis method as a ma_method choice
   #     2. Add the associated conditional panel for the parameters
   #     3. Add the auxiliary function to obtain the ordination
   #     4. Add the plotting function within reactMaPlot
-  
+
   # Functions to make the data to plot
   pca_ord <- function(data_in){
     pca_res = rda(data_in)
     return(pca_res)
   }
-  
+
   rda_ord <- function(data_in,const_var){
     v_ <- as.matrix(const_var)
     rda_res = rda(data_in~v_)
     return(rda_res)
   }
-  
+
   pcoa_ord <- function(data_in){
     pcoa_res = wcmdscale(vegdist(data_in, method=input$dist_pcoa))
     return(pcoa_res)
   }
-  
+
   nmds_ord <- function(data_in){
     nmds_res = metaMDS(data_in)
     return(nmds_res)
   }
-  
+
   ca_ord <- function(data_in){
     ca_res = cca(data_in)
     return(ca_res)
   }
-  
+
   cca_ord <- function(data_in,const_var){
     v_ <- as.matrix(const_var)
     cca_res = cca(data_in~v_)
     return(cca_res)
   }
-  
+
   # Load Data Multivariate Analysis ----
-  
+
   # Select and load dataset
   # Update filters
   observeEvent(input$load_ma,{
@@ -506,7 +502,7 @@ server <- function(input, output, clientData, session) {
       output$out_dataset_ma <- renderText(isolate(input$dataset_ma))
     }
   }) # Close load_ma observer
-  
+
   # Select and load constrained variables
   # Update filters
   observeEvent(input$load_const_ma,{
@@ -530,7 +526,7 @@ server <- function(input, output, clientData, session) {
                            selected = colnames(reactiveData$temp_ma_const_data))
     }
   }) # Close load_const_ma observer
-  
+
   # Filter and Generate Tables for Multivariate Analysis ----
   # Reactive functions to generate the data when action buttons are hit
   # Generates the table with the raw data
@@ -556,7 +552,7 @@ server <- function(input, output, clientData, session) {
     reactiveData$curr_ma_data <- reactMaData() # Analysed data
     reactiveData$org_ma_data <- reactMaData() # Displayed data
     })
-  
+
   # Generates the table with imputed values
   reactMaData_impute <- eventReactive(input$imp_ma,{
     ma_data <- isolate(reactiveData$curr_ma_data)
@@ -575,7 +571,7 @@ server <- function(input, output, clientData, session) {
     reactiveData$curr_ma_data <- reactMaData_impute() # Analysed data
     reactiveData$imp_ma_data <- reactMaData_impute() # Displayed data
     })
-  
+
   # Generates the table with transformed values
   reactMaData_transform <- eventReactive(input$trans_ma,{
     ma_data <- isolate(reactiveData$curr_ma_data)
@@ -593,7 +589,7 @@ server <- function(input, output, clientData, session) {
     reactiveData$curr_ma_data <- reactMaData_transform() # Analysed data
     reactiveData$trans_ma_data <- reactMaData_transform() # Displayed data
   })
-  
+
   # Generates the table with the selected constrained variables
   reactMaConst <- eventReactive(input$filter_const_ma,{
     if (input$const_ma == "Current project"){
@@ -604,7 +600,7 @@ server <- function(input, output, clientData, session) {
         ma_const_abun <- rownames(mostAbundant(ma_const_data, input$n_const_ma+1)) # +1 because Unclassified will be removed
         ma_const_abun <- ma_const_abun[ma_const_abun!='Unclassified']
         ma_const_data <- t(ma_const_data[ma_const_abun,input$samples_const_ma])
-      } 
+      }
     } else {
       ma_const_data <- reactiveData$temp_ma_const_data
       ma_const_data[is.na(ma_const_data)] <- 0
@@ -633,7 +629,7 @@ server <- function(input, output, clientData, session) {
     reactiveData$curr_const_data <- reactMaConst() # Analysed data
     reactiveData$org_const_data <- reactMaConst() # Displayed data
   })
-  
+
   # Generates the table with imputed contrained variables
   reactMaConst_impute <- eventReactive(input$imp_const_ma,{
     ma_const_data <- isolate(reactiveData$curr_const_data)
@@ -654,7 +650,7 @@ server <- function(input, output, clientData, session) {
     reactiveData$curr_const_data <- reactMaConst_impute() # Analysed data
     reactiveData$imp_const_data <- reactMaConst_impute() # Displayed data
   })
-  
+
   # Generates the table with transformed values
   reactMaConst_transform <- eventReactive(input$trans_const_ma,{
     ma_const_data <- isolate(reactiveData$curr_const_data)
@@ -682,29 +678,29 @@ server <- function(input, output, clientData, session) {
   output$table_imp_ma <- DT::renderDataTable({
     DT::datatable(reactiveData$imp_ma_data)
   })
-    
+
   output$table_trans_ma <- DT::renderDataTable({
     DT::datatable(reactiveData$trans_ma_data)
   })
-  
+
   output$table_const <- DT::renderDataTable({
     DT::datatable(reactiveData$org_const_data)
   })
-  
+
   output$table_imp_const_ma <- DT::renderDataTable({
     DT::datatable(reactiveData$imp_const_data)
   })
-  
+
   output$table_trans_const_ma <- DT::renderDataTable({
     DT::datatable(reactiveData$trans_const_data)
   })
-  
+
   # Reactive Plotting Function
   # try ggrepel for not overlapping text labels (works with ggplot)
   reactMaPlot <- eventReactive(input$ma_plot,{
     if (input$ma_method == "PCA"){
       pca_result <- pca_ord(reactiveData$curr_ma_data)
-      plot(pca_result,type = "n", 
+      plot(pca_result,type = "n",
            xlab = paste0(
              "PC1 (",round(summary(pca_result)$cont$importance[2,1],2)*100,"%)"
            ), ylab = paste0(
@@ -719,7 +715,7 @@ server <- function(input, output, clientData, session) {
       #print(isolate(str(reactiveData$curr_const_data)))
       #print(isolate(str(reactiveData$curr_ma_data)))
       #print(summary(str(rda_result)))
-      plot(rda_result,type = "t", 
+      plot(rda_result,type = "t",
            xlab = paste0(
              "RDA1 (",round(summary(rda_result)$cont$importance[2,1],2)*100,"%)"
            ), ylab = paste0(
@@ -742,22 +738,22 @@ server <- function(input, output, clientData, session) {
       plot(cca_ord(reactiveData$curr_ma_data,reactiveData$curr_const_data), type = 't')
     }
   })
-  
+
   output$maPlot <- renderPlot({
     print(reactMaPlot())
   })
-  
+
   output$maPlotDown <- downloadHandler(
-    # Should be able to use the reactive functions 
+    # Should be able to use the reactive functions
     # but this generates nothing
     filename = function() {
-      paste("Ma_plot_", Sys.time(), '.pdf', sep='') 
+      paste("Ma_plot_", Sys.time(), '.pdf', sep='')
     },
     content = function(file) {
       pdf(file = file)
       if (input$ma_method == "PCA"){
         pca_result <- pca_ord(reactiveData$curr_ma_data)
-        plot(pca_result,type = "n", 
+        plot(pca_result,type = "n",
              xlab = paste0(
                "PC1 (",round(summary(pca_result)$cont$importance[2,1],2)*100,"%)"
              ), ylab = paste0(
@@ -772,7 +768,7 @@ server <- function(input, output, clientData, session) {
         #print(isolate(str(reactiveData$curr_const_data)))
         #print(isolate(str(reactiveData$curr_ma_data)))
         #print(summary(str(rda_result)))
-        plot(rda_result,type = "t", 
+        plot(rda_result,type = "t",
              xlab = paste0(
                "RDA1 (",round(summary(rda_result)$cont$importance[2,1],2)*100,"%)"
              ), ylab = paste0(
